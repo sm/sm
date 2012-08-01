38f155e42ad785dac428b02475593bc1be0b3313
Michal Papis <mpapis@gmail.com>
2012-08-01 06:36:35 +0200

    add test for: log error

--- core/sm/shell/log/log_comment_test.sh
+++ core/sm/shell/log/log_comment_test.sh
@@ -6,3 +6,5 @@ log "one"         # match=/^one$/; status=0
 
 log warn "two"    # match=/^WARNING: two$/; status=0
 
+## this one is overwritten, by default it would exit shell - which would break the test
+log error "three" # match=/^ERROR\(log\): three$/; status=122
