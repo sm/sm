afe9b0b5a09001c9b001769b521a7abf8a83a48f
Michal Papis <mpapis@gmail.com>
2012-08-03 05:26:13 +0200

    commenting tests

--- core/internal/shell/files/files_comment_test.sh
+++ core/internal/shell/files/files_comment_test.sh
@@ -3,8 +3,10 @@
 source   test-sm
 includes internal/files
 
+: readlinks
 touch "${sm_temp_path}/a"
 ln -s "${sm_temp_path}/a" "${sm_temp_path}/b"
 ln -s "${sm_temp_path}/b" "${sm_temp_path}/c"
 
+## reading /c should resolve to /a
 __sm.files.readlinks "${sm_temp_path}/c" # match=/\/a$/
