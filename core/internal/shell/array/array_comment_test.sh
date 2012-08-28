eb490c8ceee1582a57d84a6e4341cf1467074bf7
Michal Papis <mpapis@gmail.com>
2012-08-28 08:25:07 +0200

    fix array pop with 3+ elements

--- core/internal/shell/array/array_comment_test.sh
+++ core/internal/shell/array/array_comment_test.sh
@@ -6,12 +6,17 @@ typeset -ga arr1
 typeset var1
 arr1=()
 
-__sm.array.length arr1   # match=/^0$/
-__sm.array.push   arr1 a # status=0
-__sm.array.length arr1   # match=/^1$/
-__sm.array.push   arr1 b # status=0
-__sm.array.length arr1   # match=/^2$/
-__sm.array.pop    arr1   # match=/^b$/; status=0
-__sm.array.length arr1   # match=/^1$/
-__sm.array.pop    arr1   # match=/^a$/; status=0
-__sm.array.length arr1   # match=/^0$/
+: push-pop-length test
+__sm.array.length arr1       # match=/^0$/
+__sm.array.push   arr1 a     # status=0
+__sm.array.length arr1       # match=/^1$/
+__sm.array.push   arr1 b     # status=0
+__sm.array.length arr1       # match=/^2$/
+__sm.array.push   arr1 "c d" # status=0
+__sm.array.length arr1       # match=/^3$/
+__sm.array.pop    arr1       # match=/^c d$/; status=0
+__sm.array.length arr1       # match=/^2$/
+__sm.array.pop    arr1       # match=/^b$/; status=0
+__sm.array.length arr1       # match=/^1$/
+__sm.array.pop    arr1       # match=/^a$/; status=0
+__sm.array.length arr1       # match=/^0$/
